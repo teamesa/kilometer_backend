6bfcbee81fb7139b743ff829c1e20dca6694ad49
chanhl22 <chanhlee@student.42seoul.kr>
2022-02-15 21:12:13 +0900

    first commit

---
+++ src/main/java/hello/easyseeannouncements/EasySeeAnnouncementsApplication.java
@@ -0,0 +1,13 @@
+package hello.easyseeannouncements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySeeAnnouncementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasySeeAnnouncementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/easyseeannouncements/EasySeeAnnouncementsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.easyseeannouncements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySeeAnnouncementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
