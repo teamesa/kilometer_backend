52b2445964a1eabab8386642c41ccccea053fbf1
noahlee <azxca1731@gmail.com>
2022-04-13 21:45:06 +0900

    make backoffice Module

---
+++ esa-backend-backoffice/src/test/java/com/esa/backend/EasySeeAnnouncementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
