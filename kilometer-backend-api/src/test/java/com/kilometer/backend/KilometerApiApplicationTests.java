80a647558d492fa2b5465cd954cc5d751a8fd922
noahlee <azxca1731@gmail.com>
2022-05-09 23:26:33 +0900

    remove unused test

--- kilometer-backend-api/src/test/java/com/kilometer/backend/KilometerApiApplicationTests.java
+++ kilometer-backend-api/src/test/java/com/kilometer/backend/KilometerApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.kilometer.backend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KilometerApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.kilometer.backend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KilometerApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
