f03a94987b2fa2345744d7ea051cc71b53f34c76
noahlee <azxca1731@gmail.com>
2022-05-11 01:32:24 +0900

    add default preconditions

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/search/SearchService.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/search/SearchService.java
@@ -2,11 +2,13 @@ package com.kilometer.domain.search;
 
 import com.kilometer.domain.search.dto.SearchRequest;
 import com.kilometer.domain.search.dto.SearchResponse;
+import org.junit.platform.commons.util.Preconditions;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SearchService {
     public SearchResponse search(SearchRequest searchRequest) {
+        Preconditions.notNull(searchRequest, String.format("this service can not be run will null object, please check this, %s", searchRequest));
         return SearchResponse.builder().build();
     }
 }
--- kilometer-backend-domain/src/test/groovy/com/kilometer/domain/search/SearchServiceTest.groovy
+++ kilometer-backend-domain/src/test/groovy/com/kilometer/domain/search/SearchServiceTest.groovy
@@ -1,6 +1,7 @@
 package com.kilometer.domain.search
 
 import com.kilometer.domain.search.dto.SearchRequest
+import org.junit.platform.commons.PreconditionViolationException
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -19,5 +20,14 @@ class SearchServiceTest extends Specification {
         def result = sut.search(insert)
         then:
         result
+    }
+
+    def "Search can not be work with null"() {
+        given:
+        def insert = null
+        when:
+        sut.search(insert)
+        then:
+        thrown(PreconditionViolationException.class)
     }
 }
