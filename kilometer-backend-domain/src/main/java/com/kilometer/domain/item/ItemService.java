fef1da94a3a0c859ac102d81012c2813a6ac6681
이동호 <edh1021@gmail.com>
2022-05-10 22:26:46 +0900

    test : Preconditions.notNull을 이용해 itemId == null 인 경우 체크 - update에서 requestDto가 null일 경우도 포함

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/item/ItemService.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/item/ItemService.java
@@ -21,56 +21,63 @@ public class ItemService {
 
     public void saveItem(ItemSaveRequest item) {
         ItemEntity itemEntity = ItemEntity.builder()
-                .exhibitionType(item.getExhibitionType())
-                .progressType(item.getProgressType())
-                .image(item.getImage())
-                .title(item.getTitle())
-                .startDate(item.getStartDate())
-                .endDate(item.getEndDate())
-                .place(item.getPlace())
-                .latitude(item.getLatitude())
-                .longitude(item.getLongitude())
-                .regionType(item.getRegionType())
-                .place(item.getPlace())
-                .fee(item.getFee())
-                .price(item.getPrice())
-                .url(item.getUrl())
-                .time(item.getTime())
-                .ticketUrl(item.getTicketUrl())
-                .build();
+            .exhibitionType(item.getExhibitionType())
+            .progressType(item.getProgressType())
+            .image(item.getImage())
+            .title(item.getTitle())
+            .startDate(item.getStartDate())
+            .endDate(item.getEndDate())
+            .place(item.getPlace())
+            .latitude(item.getLatitude())
+            .longitude(item.getLongitude())
+            .regionType(item.getRegionType())
+            .place(item.getPlace())
+            .fee(item.getFee())
+            .price(item.getPrice())
+            .url(item.getUrl())
+            .time(item.getTime())
+            .ticketUrl(item.getTicketUrl())
+            .build();
         ItemEntity savedItem = itemRepository.save(itemEntity);
     }
 
     public List<ItemResponse> findItems() {
         return itemRepository.findAll()
-                .stream()
-                .map(ItemEntity::makeResponse)
-                .collect(Collectors.toList());
+            .stream()
+            .map(ItemEntity::makeResponse)
+            .collect(Collectors.toList());
     }
 
     public ItemResponse findOne(Long itemId) {
+        Preconditions.notNull(itemId, "id must not be null");
+
         ItemEntity itemEntity = itemRepository.findById(itemId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + itemId));
+            .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + itemId));
         return itemEntity.makeResponse();
     }
 
-  public Optional<SummaryResponse> findToSummaryResponseById(Long itemId) {
-    Preconditions.notNull(itemId, "id must not be null");
-    return itemRepository.findById(itemId).map(ItemEntity::makeSummaryResponse);
-  }
+    public Optional<SummaryResponse> findToSummaryResponseById(Long itemId) {
+        Preconditions.notNull(itemId, "id must not be null");
 
-  @Transactional
-  public void updateItem(Long itemId, ItemUpdateRequest item) {
-    ItemEntity itemEntity = itemRepository.findById(itemId)
-        .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + itemId));
-    itemEntity.update(item);
-  }
+        return itemRepository.findById(itemId).map(ItemEntity::makeSummaryResponse);
+    }
 
-  public void deleteItem(Long itemId) {
-    ItemEntity itemEntity = itemRepository.findById(itemId)
-        .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + itemId));
-    itemRepository.delete(itemEntity);
-  }
+    @Transactional
+    public void updateItem(Long itemId, ItemUpdateRequest item) {
+        Preconditions.notNull(itemId, "id must not be null");
+        Preconditions.notNull(item, "item must not be null");
+
+        ItemEntity itemEntity = itemRepository.findById(itemId)
+            .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + itemId));
+        itemEntity.update(item);
+    }
+
+    public void deleteItem(Long itemId) {
+        Preconditions.notNull(itemId, "id must not be null");
+        ItemEntity itemEntity = itemRepository.findById(itemId)
+            .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + itemId));
+        itemRepository.delete(itemEntity);
+    }
 
     public List<ItemEntity> findItemsAndDetail() {
         List<ItemEntity> findItem = itemRepository.findAll();
