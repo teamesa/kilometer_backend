0df2e9383bf177964a57878436ffecb7b5ad32f5
noahlee <azxca1731@gmail.com>
2022-05-15 01:07:32 +0900

    make badge generate login

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/search/badge/ListItemBadgeGenerator.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/search/badge/ListItemBadgeGenerator.java
@@ -1,12 +1,18 @@
 package com.kilometer.domain.search.badge;
 
+import com.kilometer.domain.item.FeeType;
 import com.kilometer.domain.item.dto.ItemResponse;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
 public class ListItemBadgeGenerator {
+    private static final String ONGOING_PROGRESS_TYPE_TEXT = "ON";
+    private static final String END_PROGRESS_TYPE_TEXT = "OFF";
+    private static final String UPCOMING_PROGRESS_TYPE_TEXT = "UPCOMING";
+
     public ListItemBadge generateTypeListItemBadge(ItemResponse itemResponse) {
         return ListItemBadge.builder()
                 .isTypeBadge(true)
@@ -15,6 +21,34 @@ public class ListItemBadgeGenerator {
     }
 
     public List<ListItemBadge> generateAdditionalItemBadgeList(ItemResponse itemResponse) {
-        return List.of();
+        ListItemBadge progressTypeBadge = makeProgressTypeBadge(itemResponse.getStartDate(), itemResponse.getEndDate());
+        ListItemBadge feeTypeBadge = makeFeeTypeBadge(itemResponse.getFee());
+
+        return List.of(progressTypeBadge, feeTypeBadge);
+    }
+
+    private ListItemBadge makeProgressTypeBadge(LocalDate startDate, LocalDate endDate) {
+        LocalDate now = LocalDate.now();
+
+        String text;
+        if (now.isBefore(startDate)) {
+            text = UPCOMING_PROGRESS_TYPE_TEXT;
+        } else if (now.isAfter(endDate)) {
+            text = END_PROGRESS_TYPE_TEXT;
+        } else {
+            text = ONGOING_PROGRESS_TYPE_TEXT;
+        }
+
+        return ListItemBadge.builder()
+                .isTypeBadge(false)
+                .text(text)
+                .build();
+    }
+
+    private ListItemBadge makeFeeTypeBadge(FeeType feeType) {
+        return ListItemBadge.builder()
+                .isTypeBadge(false)
+                .text(feeType.getDescription())
+                .build();
     }
 }
