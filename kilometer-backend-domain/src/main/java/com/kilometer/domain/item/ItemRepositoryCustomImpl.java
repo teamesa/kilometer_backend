1bff735e05620f6e2d47f5a66d068abfc0beb8d0
noahlee <azxca1731@gmail.com>
2022-05-20 08:44:12 +0900

    make detail filter options

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/item/ItemRepositoryCustomImpl.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/item/ItemRepositoryCustomImpl.java
@@ -1,6 +1,7 @@
 package com.kilometer.domain.item;
 
 import com.kilometer.domain.search.request.FilterOptions;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -8,7 +9,11 @@ import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
+import static com.kilometer.domain.search.request.ProgressDateType.*;
 
 public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
     private final JPAQueryFactory queryFactory;
@@ -23,8 +28,61 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
         List<ItemEntity> items = queryFactory
                 .select(itemEntity)
                 .from(itemEntity)
+                .where(
+                        eqExhibitionType(filterOptions),
+                        eqFeeType(filterOptions),
+                        eqRegionType(filterOptions),
+                        loeStartDateNow(filterOptions),
+                        goeEndDateNow(filterOptions),
+                        goeStartDateNowLoeEndDateNow(filterOptions)
+                )
                 .fetch();
 
         return new PageImpl<>(items, pageable, items.size());
+    }
+
+    private BooleanExpression eqExhibitionType(FilterOptions filterOptions) {
+        return Optional.ofNullable(filterOptions)
+                .map(FilterOptions::getExhibitionType)
+                .map(itemEntity.exhibitionType::eq)
+                .orElse(null);
+    }
+
+    private BooleanExpression eqFeeType(FilterOptions filterOptions) {
+        return Optional.ofNullable(filterOptions)
+                .map(FilterOptions::getFeeType)
+                .map(itemEntity.fee::eq)
+                .orElse(null);
+    }
+
+    private BooleanExpression eqRegionType(FilterOptions filterOptions) {
+        return Optional.ofNullable(filterOptions)
+                .map(FilterOptions::getRegionType)
+                .map(itemEntity.regionType::eq)
+                .orElse(null);
+    }
+
+    private BooleanExpression loeStartDateNow(FilterOptions filterOptions) {
+        return Optional.ofNullable(filterOptions)
+                .map(FilterOptions::getProgressType)
+                .filter(UPCOMING::equals)
+                .map(it -> itemEntity.startDate.loe(LocalDate.now()))
+                .orElse(null);
+    }
+
+    private BooleanExpression goeEndDateNow(FilterOptions filterOptions) {
+        return Optional.ofNullable(filterOptions)
+                .map(FilterOptions::getProgressType)
+                .filter(OFF::equals)
+                .map(it -> itemEntity.endDate.goe(LocalDate.now()))
+                .orElse(null);
+    }
+
+    private BooleanExpression goeStartDateNowLoeEndDateNow(FilterOptions filterOptions) {
+        return Optional.ofNullable(filterOptions)
+                .map(FilterOptions::getProgressType)
+                .filter(ON::equals)
+                .map(it -> itemEntity.startDate.goe(LocalDate.now()).and(itemEntity.endDate.loe(LocalDate.now())))
+                .orElse(null);
     }
 }
--- kilometer-backend-domain/src/main/java/com/kilometer/domain/search/request/FilterOptions.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/search/request/FilterOptions.java
@@ -1,16 +1,17 @@
 package com.kilometer.domain.search.request;
 
+import com.kilometer.domain.item.ExhibitionType;
 import com.kilometer.domain.item.FeeType;
 import com.kilometer.domain.item.RegionType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
 public class FilterOptions {
+    private ExhibitionType exhibitionType;
     private ProgressDateType progressType;
     private FeeType feeType;
     private RegionType regionType;
