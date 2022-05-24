e04b333681cf8494756545219e885426a3135377
chanhl22 <lch9502@naver.com>
2022-05-24 22:06:40 +0900

    Enum 수정 - 롬복 추가

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/item/ExposureType.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/item/ExposureType.java
@@ -1,16 +1,13 @@
 package com.kilometer.domain.item;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ExposureType {
 
     ON("전시"), OFF("미전시");
 
     private final String description;
-
-    ExposureType(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
