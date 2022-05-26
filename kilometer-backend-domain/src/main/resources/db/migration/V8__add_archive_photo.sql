195066b2936828bb0f49464278be5f8aa9a09cce
이동호 <edh1021@gmail.com>
2022-05-27 01:38:40 +0900

    feat : archive 사진 DB 추가

--- kilometer-backend-domain/src/main/resources/db/migration/V8__add_archive_photo.sql
+++ kilometer-backend-domain/src/main/resources/db/migration/V8__add_archive_photo.sql
@@ -2,8 +2,9 @@ CREATE TABLE IF NOT EXISTS archive_photo
 (
     id          BIGINT(20)   NOT NULL AUTO_INCREMENT,
     archiveId   BIGINT(20)   NULL,
-    imageUrl   VARCHAR(255) NOT NULL
+    imageUrl   VARCHAR(255) NOT NULL,
 
     PRIMARY KEY (id),
-    CONSTRAINT archive_photo-archive-fk FOREIGN KEY (archiveId) REFERENCES archive (id)
-    )
+    CONSTRAINT archive_photo_archive_fk FOREIGN KEY (archiveId) REFERENCES archive (id)
+        ON DELETE CASCADE
+    );
