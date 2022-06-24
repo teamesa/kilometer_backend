c0cfdec4614f5ca12eb23ace08d43dcd3bde8469
chanhl22 <lch9502@naver.com>
2022-06-24 22:22:17 +0900

    백오피스 리팩토링 * 수정

--- kilometer-backend-backoffice/src/main/java/com/kilometer/backend/controller/ItemController.java
+++ kilometer-backend-backoffice/src/main/java/com/kilometer/backend/controller/ItemController.java
@@ -1,6 +1,10 @@
 package com.kilometer.backend.controller;
 
-import com.kilometer.domain.item.*;
+import com.kilometer.domain.item.ExhibitionType;
+import com.kilometer.domain.item.ExposureType;
+import com.kilometer.domain.item.FeeType;
+import com.kilometer.domain.item.ItemService;
+import com.kilometer.domain.item.RegionType;
 import com.kilometer.domain.item.dto.ItemRequest;
 import com.kilometer.domain.item.dto.ItemResponse;
 import com.kilometer.domain.util.BoUrlUtils;
@@ -8,7 +12,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
