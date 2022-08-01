d0d51a5a6162175cb30aa54086ff1a0c42772fae
이동호 <edh1021@gmail.com>
2022-08-02 00:06:21 +0900

    Archive detail update link 수정 -> Front router url

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/util/FrontUrlUtils.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/util/FrontUrlUtils.java
@@ -1,12 +1,13 @@
 package com.kilometer.domain.util;
 
 public class FrontUrlUtils {
+
     private static final String FRONT_DETAIL = "/detail/";
     private static final String FRONT_ARCHIVE = "/archive";
     private static final String FRONT_DETAIL_URL_PATTERN = FRONT_DETAIL + "%s";
-
     private static final String FRONT_MY_ARCHIVE_TITLE = "MY 아카이브";
-    private static final String FRONT_MY_ARCHIVE_TITLE_PATTERN = FRONT_MY_ARCHIVE_TITLE+"(%d)";
+    private static final String FRONT_MY_ARCHIVE_TITLE_PATTERN = FRONT_MY_ARCHIVE_TITLE + "(%d)";
+    private static final String FRONT_ARCHIVE_MODIFY = FRONT_ARCHIVE + "/update";
 
     public static String getFrontDetailUrlPattern(long itemId) {
         return String.format(FRONT_DETAIL_URL_PATTERN, itemId);
@@ -17,12 +18,18 @@ public class FrontUrlUtils {
     }
 
     public static String getFrontMyArchiveTitlePattern(long count) {
-        return String.format(FRONT_MY_ARCHIVE_TITLE_PATTERN,count);
+        return String.format(FRONT_MY_ARCHIVE_TITLE_PATTERN, count);
     }
 
     public static String getFrontMyArchiveTitle() {
         return FRONT_MY_ARCHIVE_TITLE;
     }
 
-    public static String getFrontArchivePrefix() { return FRONT_ARCHIVE;}
+    public static String getFrontArchivePrefix() {
+        return FRONT_ARCHIVE;
+    }
+
+    public static String getFrontArchiveModify() {
+        return FRONT_ARCHIVE_MODIFY;
+    }
 }
