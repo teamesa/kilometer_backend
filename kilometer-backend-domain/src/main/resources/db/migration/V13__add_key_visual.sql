e338651ee40ca3641f1d2f39aaa6d461007d86f1
chanhl22 <lch9502@naver.com>
2022-09-01 14:51:25 +0900

    Home 데이터베이스 수정

--- kilometer-backend-domain/src/main/resources/db/migration/V13__add_key_visual.sql
+++ kilometer-backend-domain/src/main/resources/db/migration/V13__add_key_visual.sql
@@ -2,24 +2,24 @@ CREATE TABLE IF NOT EXISTS key_visual
 (
     id              BIGINT(20)      NOT NULL AUTO_INCREMENT,
 
-    imageUrl        VARCHAR(3000)   NULL,
-    title           VARCHAR(255)    NULL,
-    subtitle        VARCHAR(255)    NULL,
-    homepageUrl     VARCHAR(3000)   NULL,
+    imageUrl        VARCHAR(3000)   NOT NULL,
+    upperTitle      VARCHAR(255)    NULL,
+    lowerTitle      VARCHAR(255)    NULL,
+    linkUrl         VARCHAR(3000)   NULL,
 
-    updateAccount   VARCHAR(255)    NULL,
-    updatedAt       DATETIME        NOT NULL DEFAULT current_timestamp,
+    createdAccount  VARCHAR(255)    NULL,
+    createdAt       DATETIME        NOT NULL DEFAULT current_timestamp,
 
     PRIMARY KEY (id)
 );
 
-INSERT INTO key_visual(imageUrl, title, subtitle, homepageUrl, updateAccount) VALUES (null, null, null, null, null);
-INSERT INTO key_visual(imageUrl, title, subtitle, homepageUrl, updateAccount) VALUES (null, null, null, null, null);
-INSERT INTO key_visual(imageUrl, title, subtitle, homepageUrl, updateAccount) VALUES (null, null, null, null, null);
-INSERT INTO key_visual(imageUrl, title, subtitle, homepageUrl, updateAccount) VALUES (null, null, null, null, null);
-INSERT INTO key_visual(imageUrl, title, subtitle, homepageUrl, updateAccount) VALUES (null, null, null, null, null);
-INSERT INTO key_visual(imageUrl, title, subtitle, homepageUrl, updateAccount) VALUES (null, null, null, null, null);
-INSERT INTO key_visual(imageUrl, title, subtitle, homepageUrl, updateAccount) VALUES (null, null, null, null, null);
-INSERT INTO key_visual(imageUrl, title, subtitle, homepageUrl, updateAccount) VALUES (null, null, null, null, null);
-INSERT INTO key_visual(imageUrl, title, subtitle, homepageUrl, updateAccount) VALUES (null, null, null, null, null);
-INSERT INTO key_visual(imageUrl, title, subtitle, homepageUrl, updateAccount) VALUES (null, null, null, null, null);
+INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
+INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
+INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
+INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
+INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
+INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
+INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
+INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
+INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
+INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
