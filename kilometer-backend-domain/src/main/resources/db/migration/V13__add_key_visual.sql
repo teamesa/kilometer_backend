a5ff16b6653a8d7b8ee64b50cdb6b14af2816c99
chanhl22 <lch9502@naver.com>
2022-09-06 09:18:51 +0900

    데이터베이스 수정 > 링크 VARCHAR 길이 축소

--- kilometer-backend-domain/src/main/resources/db/migration/V13__add_key_visual.sql
+++ kilometer-backend-domain/src/main/resources/db/migration/V13__add_key_visual.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS key_visual
 (
     id              BIGINT(20)      NOT NULL AUTO_INCREMENT,
 
-    imageUrl        VARCHAR(3000)   NOT NULL,
+    imageUrl        VARCHAR(255)   NOT NULL,
     upperTitle      VARCHAR(255)    NULL,
     lowerTitle      VARCHAR(255)    NULL,
-    linkUrl         VARCHAR(3000)   NULL,
+    linkUrl         VARCHAR(255)   NULL,
 
     createdAccount  VARCHAR(255)    NULL,
     createdAt       DATETIME        NOT NULL DEFAULT current_timestamp,
@@ -23,3 +23,8 @@ INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount
 INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
 INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
 INSERT INTO key_visual(imageUrl, upperTitle, lowerTitle, linkUrl, createdAccount) VALUES ("", null, null, null, null);
+
+ALTER TABLE item MODIFY listImageUrl VARCHAR(255);
+ALTER TABLE item MODIFY thumbnailImageUrl VARCHAR(255);
+ALTER TABLE item MODIFY homepageUrl VARCHAR(255);
+ALTER TABLE item MODIFY ticketUrl VARCHAR(255);
