1c106ced1daaa66236ea35e72b92ab97577563d9
이동호 <edh1021@gmail.com>
2022-11-06 18:33:17 +0900

    fix : 아카이브 평균 별점 자리수 수정

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/archive/generator/ArchiveRatingConverter.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/archive/generator/ArchiveRatingConverter.java
@@ -5,7 +5,10 @@ public class ArchiveRatingConverter {
 
     private final static double ROUND_DECIMAL_PLACE = 10.0;
 
-    public static double convertArchiveRatingAverage(double archiveRatingAverage) {
+    public static double convertArchiveRatingAverage(Double archiveRatingAverage) {
+        if (archiveRatingAverage == null) {
+            return 0.0;
+        }
         return Math.round(archiveRatingAverage * ROUND_DECIMAL_PLACE) / ROUND_DECIMAL_PLACE;
     }
 }
