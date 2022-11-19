736cc87bde824b192e58e86f80e53017f1c76680
KODO <edh1021@gmail.com>
2022-11-19 20:19:32 +0900

    fix : homeApi keyvisual response 변경 (#185) (#187)

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/homeModules/modules/keyVisual/KeyVisualHandler.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/homeModules/modules/keyVisual/KeyVisualHandler.java
@@ -2,6 +2,7 @@ package com.kilometer.domain.homeModules.modules.keyVisual;
 
 import com.kilometer.domain.homeModules.enumType.ModuleType;
 import com.kilometer.domain.homeModules.modules.ModuleHandler;
+import com.kilometer.domain.homeModules.modules.keyVisual.dto.KeyVisualApiResponse;
 import com.kilometer.domain.homeModules.modules.keyVisual.dto.KeyVisualDataDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -25,8 +26,10 @@ public class KeyVisualHandler implements ModuleHandler {
     @Override
     public Object generator(String data) {
         List<KeyVisual> keyVisuals = keyVisualRepository.findAllOrderByIdAtAsc();
-        return keyVisuals.stream()
+        return KeyVisualApiResponse.from(
+            keyVisuals.stream()
             .map(KeyVisualDataDto::from)
-            .collect(Collectors.toList());
+            .collect(Collectors.toList())
+        );
     }
 }
