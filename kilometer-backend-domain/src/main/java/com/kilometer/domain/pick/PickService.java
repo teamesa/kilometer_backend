2adcccc238616b1043b9eadf506e4f661e983b8e
chanhl22 <lch9502@naver.com>
2022-12-16 19:39:18 +0900

    Pick API > QueryDSL 수정

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/pick/PickService.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/pick/PickService.java
@@ -5,9 +5,8 @@ import com.kilometer.domain.converter.listItem.ListItemAggregateConverter;
 import com.kilometer.domain.converter.listItem.dto.ListItem;
 import com.kilometer.domain.item.ItemEntity;
 import com.kilometer.domain.item.ItemService;
-import com.kilometer.domain.item.dto.ItemResponse;
 import com.kilometer.domain.paging.PagingStatusService;
-import com.kilometer.domain.pick.dto.MostPickItem;
+import com.kilometer.domain.pick.dto.MostPickItemDto;
 import com.kilometer.domain.pick.dto.MostPickResponse;
 import com.kilometer.domain.pick.dto.MyPickResponse;
 import com.kilometer.domain.pick.dto.PickItemResponse;
@@ -15,7 +14,6 @@ import com.kilometer.domain.pick.dto.PickRequest;
 import com.kilometer.domain.pick.dto.PickResponse;
 import com.kilometer.domain.user.User;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -80,21 +78,16 @@ public class PickService {
 
     @Transactional
     public MostPickResponse getMostPicks() {
-        List<MostPickItem> mostPickTop4 = pickRepository.findByMostPickTop4(
-                LocalDateTime
-                .of(LocalDateTime.now().getYear(), LocalDateTime.now().getMonth(), LocalDateTime.now().getDayOfMonth(), 0, 0, 0)
-                .withDayOfMonth(1));
+        List<MostPickItemDto> mostPickTop4 = pickRepository.findMostPickTop4(
+                LocalDateTime.of(LocalDateTime.now().getYear(), LocalDateTime.now().getMonth(),
+                                LocalDateTime.now().getDayOfMonth(), 0, 0, 0)
+                        .withDayOfMonth(1));
 
-        List<Long> idList = mostPickTop4.stream()
-                .map(MostPickItem::getPickedItem)
-                .collect(Collectors.toList());
-
-        return convertToMostPick(itemService.findByIdIn(idList));
+        return convertToMostPick(mostPickTop4);
     }
 
-    private MostPickResponse convertToMostPick(List<ItemResponse> items) {
+    private MostPickResponse convertToMostPick(List<MostPickItemDto> items) {
         List<ListItem> itemList = items.stream()
-                .map(PickItemResponse::makePickItemResponse)
                 .map(listItemAggregateConverter::convert)
                 .collect(Collectors.toList());
 
