f46b150cf00e80bdfc4afbf30eaeaf3db91d6c6a
doho-ho <edh1021@gmail.com>
2023-01-28 00:05:31 +0900

    백엔드 API End-point(url) 변경 (#223)
    * refactor - archive endpoint 수정
    
    * refactor - api endpoint 수정
    
    * refactor - swagger api 설명 작성

--- kilometer-backend-api/src/main/java/com/kilometer/backend/controller/HomeController.java
+++ kilometer-backend-api/src/main/java/com/kilometer/backend/controller/HomeController.java
@@ -1,30 +1,31 @@
 package com.kilometer.backend.controller;
 
-import static com.kilometer.backend.security.security.SecurityUtils.getLoginUserId;
-
 import com.kilometer.domain.homeModules.HomeApiResponse;
 import com.kilometer.domain.homeModules.HomeRenderingService;
 import com.kilometer.domain.homeModules.ModuleResponseDto;
 import com.kilometer.domain.util.ApiUrlUtils;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.kilometer.backend.security.security.SecurityUtils.getLoginUserId;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(ApiUrlUtils.HOME_ROOT)
 public class HomeController {
 
     private final HomeRenderingService moduleService;
 
     @GetMapping(ApiUrlUtils.KEY_VISUAL)
+    @ApiOperation(value = "홈 모듈 중 키비주얼만 조회")
     public ModuleResponseDto getKeyVisual() {
         Long userId = getLoginUserId();
         return moduleService.getKeyVisual(userId);
     }
 
-    @GetMapping
+    @GetMapping(ApiUrlUtils.HOME_ROOT)
+    @ApiOperation(value = "홈 모듈 모두 조회")
     public HomeApiResponse homeApi() {
         Long userId = getLoginUserId();
         return moduleService.getHomeModules(userId);
--- kilometer-backend-api/src/main/java/com/kilometer/backend/controller/ItemController.java
+++ kilometer-backend-api/src/main/java/com/kilometer/backend/controller/ItemController.java
@@ -1,7 +1,5 @@
 package com.kilometer.backend.controller;
 
-import static com.kilometer.backend.security.security.SecurityUtils.getLoginUserId;
-
 import com.kilometer.domain.item.ItemService;
 import com.kilometer.domain.item.dto.DetailResponse;
 import com.kilometer.domain.item.dto.ItemInfoResponse;
@@ -11,17 +9,17 @@ import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.kilometer.backend.security.security.SecurityUtils.getLoginUserId;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(ApiUrlUtils.ITEM_ROOT)
 public class ItemController {
 
     private final ItemService itemService;
 
-    @GetMapping(ApiUrlUtils.ITEM_INFO)
+    @GetMapping(ApiUrlUtils.ITEM_ID)
     @ApiOperation(value = "전시글 기본정보 API")
     public ItemInfoResponse getInfo(
         @ApiParam(value = "조회할 전시글 ID", required = true) @PathVariable Long itemId) {
@@ -29,11 +27,22 @@ public class ItemController {
         return itemService.getItemInfo(itemId, userId);
     }
 
-    @GetMapping(ApiUrlUtils.ITEM_DETAIL)
+    /**
+     * 전시글의 소개글과 사진을 반환하는 API
+     * @deprecated 전시글 조회 api("api/items/{itemId}")로 이전
+     */
+    @Deprecated
+    @GetMapping("/api/items/{itemId}/detail")
     @ApiOperation(value = "전시글 소개 API")
     public DetailResponse getDetail(
         @ApiParam(value = "조회할 전시글 ID", required = true) @PathVariable Long itemId) {
         return itemService.getItemDetail(itemId);
+    }
+
+    @GetMapping(ApiUrlUtils.ITEM_SUMMARY)
+    @ApiOperation(value ="전시글 제목, 리스트이미지, 아카이브 작성 여부만 조회")
+    public void getSummary(@PathVariable Long itemId) {
+
     }
 
 }
