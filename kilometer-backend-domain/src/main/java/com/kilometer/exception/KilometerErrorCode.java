30a00fdc97c523f7e8b327cc97616e6782cf945f
koo <gudonghee2000@gmail.com>
2023-01-29 18:52:41 +0900

    feat: 아카이브 중복에 대한 예외처리를 KilometerErrorCode에 추가

--- kilometer-backend-domain/src/main/java/com/kilometer/exception/KilometerErrorCode.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/exception/KilometerErrorCode.java
@@ -6,7 +6,7 @@ public enum KilometerErrorCode {
 
     ARCHIVE_NOT_FOUND("ALT-017", "Archive가 존재하지 않습니다."),
     ARCHIVE_VALIDATION_EXCEPTION("ALT-018", "요청이 잘못되었습니다."),
-    ARCHIVE_DUPLICATE_EXCEPTION("ALT-", "이미 저장된 Archive가 있습니다.");
+    ARCHIVE_DUPLICATE_EXCEPTION("ALT-015", "이미 등록된 아카이브입니다.");
 
     @Getter
     private final String code;
