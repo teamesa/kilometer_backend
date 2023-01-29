1b7a5236ad92255cb7fa8f607f81c0252488c55f
skullKim <kyk990328@gmail.com>
2023-01-29 12:43:21 +0900

    chore: 필요 없는 import 삭제

--- kilometer-backend-domain/src/test/groovy/com/kilometer/common/annotation/SpringTestWithData.java
+++ kilometer-backend-domain/src/test/groovy/com/kilometer/common/annotation/SpringTestWithData.java
@@ -1,22 +1,12 @@
 package com.kilometer.common.annotation;
 
 import com.kilometer.common.support.DataClearExtension;
-import com.kilometer.domain.hello.HelloRepository;
-import com.kilometer.domain.hello.HelloService;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.test.context.ContextConfiguration;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
