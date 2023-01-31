f117732c5719645c508d5eb37672d9c5baf65ecb
koo <gudonghee2000@gmail.com>
2023-01-31 19:37:05 +0900

    refactor: LikeService.findByArchiveIdAndUserId() 메서드 지역변수 줄바꿈 추가

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/archive/like/LikeService.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/archive/like/LikeService.java
@@ -5,7 +5,6 @@ import com.kilometer.domain.archive.ArchiveEntity;
 import com.kilometer.domain.archive.like.dto.LikeDto;
 import com.kilometer.domain.user.User;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +19,12 @@ public class LikeService {
         Preconditions.checkNotNull(archiveId, "Archive id must not be null");
         Preconditions.checkNotNull(userId, "User id must not be null");
 
-        ArchiveEntity likedArchiveEntity = ArchiveEntity.builder().id(archiveId).build();
-        User likedUser = User.builder().id(userId).build();
+        ArchiveEntity likedArchiveEntity = ArchiveEntity.builder()
+            .id(archiveId)
+            .build();
+        User likedUser = User.builder()
+            .id(userId)
+            .build();
 
         return LikeDto.of(likeRepository.findByLikedArchiveEntityAndLikedUser(likedArchiveEntity, likedUser)
             .orElseGet(() -> newLike(likedArchiveEntity, likedUser)));
