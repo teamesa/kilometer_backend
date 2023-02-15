783d24bc3461aaff04b942b6722d49c9a2cd6866
koo <gudonghee2000@gmail.com>
2023-02-15 22:03:04 +0900

    refactor: ArchiveFilter의 필드를 static한 HashSet으로 변경

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/archive/domain/Archive.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/archive/domain/Archive.java
@@ -28,11 +28,10 @@ public class Archive {
     }
 
     private void validateCommentField() {
-        ArchiveFilter archiveFilter = new ArchiveFilter();
         if (this.comment == null) {
             throw new ArchiveValidationException("입력된 comment가 없습니다.");
         }
-        if (archiveFilter.isMatchWithForbiddenWords(this.comment)) {
+        if (ArchiveFilter.isMatchWithForbiddenWords(this.comment)) {
             throw new ArchiveValidationException("입력된 comment에 금칙어가 포함되어 있습니다.");
         }
     }
