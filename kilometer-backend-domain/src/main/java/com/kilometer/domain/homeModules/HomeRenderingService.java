10c6fb94670cf1086f77d5c2225a956de5a4d518
skullKim <kyk990328@gmail.com>
2023-02-16 23:49:27 +0900

    fix: 모듈에 문제가 있으면 기타 모듈도 로딩이 되지 않는 문제

--- kilometer-backend-domain/src/main/java/com/kilometer/domain/homeModules/HomeRenderingService.java
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/homeModules/HomeRenderingService.java
@@ -16,7 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class HomeRenderingService {
 
