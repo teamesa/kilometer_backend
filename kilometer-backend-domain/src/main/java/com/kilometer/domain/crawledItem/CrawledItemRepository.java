769a4fdbe5228a84839467f84fce60c20652f69f
skullKim <kyk990328@gmail.com>
2023-08-03 19:55:17 +0900

    feat: 크롤링한 데이터를 저장할 테이블

---
+++ kilometer-backend-domain/src/main/java/com/kilometer/domain/crawledItem/CrawledItemRepository.java
@@ -0,0 +1,6 @@
+package com.kilometer.domain.crawledItem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CrawledItemRepository extends JpaRepository<CrawledItem, Long> {
+}
